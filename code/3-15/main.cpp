38d18998ba09073f11b1d80f23030dd92481b45b
sinary <2470224248@qq.com>
2020-03-18 23:44:14 +0800

    s

--- code/3-15/main.cpp
+++ code/3-15/main.cpp
@@ -1,3 +1,7 @@
+/*代码功能：求两个整数的最小公倍数与最大公因数
+ *作者：孙佳伟
+ * 编辑时间：2020年3月18日23:06:55
+ * */
 #include <iostream>
 
 using namespace std;
@@ -11,40 +15,29 @@ int main() {
     if ((a == 0 && b == 0) || a < 0 || b < 0)
         cout << "输入的数据无法求最大公因数与最小公倍数" << endl;
     else {
-        if (a < b) {
-            int c = 0, d = 0;
-            for (int i = a; i >=  1; i--) {
-                if (!(a % i) && !(b % i)) {
-                    c = i;
-                    break;
-                }
-            }
-            for (int j = b; j <= a * b; j++) {
-                if (!(j % a) && !(j % b)) {
-                    d = j;
-                    break;
-                }
-            }
-            cout << "最大公因数为" << c << endl;
-            cout << "最小公倍数为" << d << endl;
+        if (a > b) {
+            int temp = a;//交换a，b；
+            a = b;
+            b = temp;
 
-        } else {
-            int c = 0, d = 0;
-            for (int i = b; i >=  1; i--) {
-                if (!(a % i) && !(b % i)) {
-                    c = i;
-                    break;
-                }
-            }
-            for (int j = a; j <= a * b; j++) {
-                if (!(j % a) && !(j % b)) {
-                    d = j;
-                    break;
-                }
-            }
-            cout << "最大公因数为" << c << endl;
-            cout << "最小公倍数为" << d << endl;
         }
+        int c = 0, d = 0;
+        for (int i = a; i >= 1; i--) {
+            if (!(a % i) && !(b % i)) {
+                c = i;
+                break;
+            }
+        }
+        for (int j = b; j <= a * b; j++) {
+            if (!(j % a) && !(j % b)) {
+                d = j;
+                break;
+            }
+        }
+        cout << "最大公因数为" << c << endl;
+        cout << "最小公倍数为" << d << endl;
+
+
     }
     return 0;
 }
