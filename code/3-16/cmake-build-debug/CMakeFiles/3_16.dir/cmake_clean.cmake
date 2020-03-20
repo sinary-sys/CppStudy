b6e8056ffa7af15ebe440eb964a1ec1f6fc17e49
sinary <2470224248@qq.com>
2020-03-21 01:02:31 +0800

    sinary

---
+++ code/3-16/cmake-build-debug/CMakeFiles/3_16.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3_16.exe"
+  "3_16.exe.manifest"
+  "3_16.lib"
+  "3_16.pdb"
+  "CMakeFiles/3_16.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_16.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ code/3-20/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_6698a.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_6698a.dir/CMakeCCompilerABI.c.obj"
+  "cmTC_6698a.exe"
+  "cmTC_6698a.exe.manifest"
+  "cmTC_6698a.lib"
+  "cmTC_6698a.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_6698a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
