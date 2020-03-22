2dbcc17a5a90c5f0970e22212b5ce0e51a287545
sinary <2470224248@qq.com>
2020-03-23 01:03:36 +0800

    sinary

---
+++ code/4-3/cmake-build-debug/CMakeFiles/3_20.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3_20.exe"
+  "3_20.exe.manifest"
+  "3_20.lib"
+  "3_20.pdb"
+  "CMakeFiles/3_20.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_20.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ code/4-3/main.cpp
@@ -0,0 +1,26 @@
+/*代码功能：判断素数
+ *
+ * */
+#include <iostream>
+using namespace std;
+/*函数功能：判断输入是否为质数
+ * 输入：number：一个整数
+ * 输出：true：是质数
+ *      false:不是质数
+ * */
+bool judge_prime_number(int number)
+{
+    for (int i = number-1; i > 1 ; i--) {
+        if (number%i==0)return false;
+    }
+    return true;
+}
+int main() {
+    int number=0;
+    cin>>number;
+    if(judge_prime_number(number))
+        cout<< "number是质数"<<endl;
+    else
+        cout<< "number不是质数"<<endl;
+    return 0;
+}
