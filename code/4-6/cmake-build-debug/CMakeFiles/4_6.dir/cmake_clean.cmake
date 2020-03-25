6154cbae70d36b470811e88b846a291e292e55f3
sinary <2470224248@qq.com>
2020-03-26 00:16:56 +0800

    sinary

---
+++ code/4-6/cmake-build-debug/CMakeFiles/4_6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "4_6.exe"
+  "4_6.exe.manifest"
+  "4_6.lib"
+  "4_6.pdb"
+  "CMakeFiles/4_6.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/4_6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
