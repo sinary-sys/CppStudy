dccaa43625ea1224f7c27432738d46dbbc8aa843
sinary <2470224248@qq.com>
2020-04-26 13:05:17 +0800

    sinary

---
+++ code/6-17/cmake-build-debug/CMakeFiles/6_17.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "6_17.exe"
+  "6_17.exe.manifest"
+  "6_17.lib"
+  "6_17.pdb"
+  "CMakeFiles/6_17.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/6_17.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ code/6-17/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int strcmp(char *p1, char *p2) {
+    int index=0;
+    for (int i = 0; i <20 ; i++) {
+        if(*(p1+i)==*(p2+i)){
+            index++;
+        } else{
+            return *(p1+i) -*(p2+i);
+        }
+    }
+    if(index==20)return 0;
+
+    return index;
+}
+
+int main() {
+    char s1[20],s2[20];
+    cout<<"please input string s1"<<endl;
+    cin>>s1;
+    cout<<"please input string s2"<<endl;
+    cin>>s2;
+    cout<<"your input string is s1="<<s1<<" "<<"s2="<<s2<<endl;
+    int a= strcmp(s1,s2);
+    cout<<a;
+
+    return 0;
+}
