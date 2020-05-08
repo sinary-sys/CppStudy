08b148b06567e24907b2d6168669aa68d51f5d54
sinary <2470224248@qq.com>
2020-05-08 20:50:03 +0800

    sinary

---
+++ code/8-6/cmake-build-debug/CMakeFiles/8_6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "8_6.exe"
+  "8_6.exe.manifest"
+  "8_6.lib"
+  "8_6.pdb"
+  "CMakeFiles/8_6.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/8_6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ code/8-6/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+class cuboid{
+public:
+
+private:
+    int length;
+    int width;
+    int height;
+};
+int main() {
+
+    return 0;
+}
