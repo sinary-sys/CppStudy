066b284e32f624b3ad69d6051f76938864543ebb
sinary <2470224248@qq.com>
2020-06-05 18:46:26 +0800

    sinary

---
+++ code/program/code/9-2—9-3/9-2—9-3.cpp
@@ -0,0 +1,55 @@
+﻿
+// 9-2—9-3.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+class Date
+{
+public:Date(int m,int d,int y):month(m),day(d),year(y){};
+	   Date(int m,int d):month(m),day(d)
+	   {
+		   year = 2005;
+	   };
+	   Date(int m):month(m)
+	   {
+		   day = 1;
+		   year = 2005;
+	   };
+	   Date()
+	   {
+		   month = 1;
+		   day = 1;
+		   year = 2005;
+	   };
+	void display()
+	{
+		cout << month << "/" << day << "/" << year << endl;
+	}
+private:int month;
+		int day;
+		int year;
+};
+int main()
+{
+	Date d1(10, 13, 2005);
+	Date d2(12, 30);
+	Date d3(10);
+	Date d4;
+	d1.display();
+	d2.display();
+	d3.display();
+	d4.display();
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
